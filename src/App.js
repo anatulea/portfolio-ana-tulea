c52d8b9ebd47b080538bf5ea18b852452c48d160
anatulea <anatulea14@.gmali.com>
2019-09-27 12:49:50 -0500

    added navigation bar and project cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Navigation from './components/Navbar.js'
+import imageName from "./img/resizedWebdev.jpg"
+import nasaPic1 from "./img/nasaPic1.jpg"
+import nasaPic2 from "./img/nasaPic2.jpg"
+
+import { Card, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle, Container, Row, Col
+} from 'reactstrap';
+
+const Picture = styled.div`
+margin : 10px 20px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Ana Tulea's portfolio.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header >
+       <Navigation></Navigation>
+       </header>
+       <Container>
+        <Row>
+          <Col></Col>
+        </Row>
+       <div className="top-image">
+         <div className="image">
+            <img src={imageName}></img>
+         </div>
+       </div>
+      
+  
+      <body>
+        <Card>
+          <CardBody>
+            <CardTitle>Astronomy Photo Of The Day</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+          </CardBody>
+          <Row>
+            <Col xs="6"> 
+              <Picture>
+                <img width="100%" src={nasaPic1} alt="Card image cap" />
+              </Picture>
+            </Col>
+          
+            <Col xs="6">
+              <Picture>
+                <img width="100%" src={nasaPic2} alt="Card image cap" />
+              </Picture>
+            </Col>
+          </Row>
+          <CardBody>
+            <CardText>The application was created using React. 
+              I used data from a NASA API and introduced it into my page.
+               I styled the application using Reactstrap and Stayled components.</CardText>
+            <CardLink href="https://ana-tulea-nasa-photo-of-the-day.netlify.com/">See my work</CardLink>
+    
+          </CardBody>
+        </Card>
+      </body>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  CardLink
+  } from 'reactstrap';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Ana Tulea</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Home</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                Projects
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                  <CardLink href="https://ana-tulea-nasa-photo-of-the-day.netlify.com/">Photo Of The Day</CardLink>
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Contact</NavLink>
+              </NavItem>
+            </Nav>
+
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
